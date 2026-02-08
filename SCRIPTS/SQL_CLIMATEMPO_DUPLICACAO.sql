183cfbd90194ad9c4fbd77581c02bc6a8c1b9697
Wagner Pires <wagnerdba@gmail.com>
2026-02-08 19:55:51 -0300

    Ajustes

--- SCRIPTS/SQL_CLIMATEMPO_DUPLICACAO.sql
+++ SCRIPTS/SQL_CLIMATEMPO_DUPLICACAO.sql
@@ -1,6 +1,7 @@
 select * from sensor_data where data_hora::date >= '2025-12-15' order by id desc; --current_date order by id desc;
 select * from sensor_data where uuid = 'b07cf602-3b6e-42f2-9af5-d376fe44b065'
 select * from sensor_data order by id desc limit 15
+select min(data_hora) from sensor_data
 
 select * from sensor_data where id >= 782354 limit 4;
 select * from sensor_data where fallback = true or id = 780670;
